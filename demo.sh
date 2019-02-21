b5660a6692e2cb9f858404abe803abfbdffd6a97
xingr <xiangxingx@gmail.com>
2019-02-21 18:34:08 +0800

    一个小脚本

---
+++ demo.sh
@@ -0,0 +1,13 @@
+if [ -d $1 ]; then
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  echo "<!DOCTYPE><title>Hello</title><h1>Hi</h1>" > 1.html
+  echo "h1{color: red;}" >style.css
+  echo "var string = "Hello World" alert(string)" > main.js
+  mv style.css css
+  mv main.js js
+ exit
+fi
