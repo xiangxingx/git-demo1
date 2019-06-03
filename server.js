2c72e5b2642dafa8b8d6d65104b76ff451951d58
xiangxingx <xiangxingx@gmail.com>
2019-06-03 21:48:30 +0800

    前后端初遇

--- server.js
+++ server.js
@@ -3,42 +3,65 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url
+  var queryString = ''
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('HTTP 路径为\n' + path)
-  if(path == '/style.css'){
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('body{background-color: #ddd;}h1{color: red;}')
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+
+  /*  if (path === '/') {
+     response.statusCode = 200
+     var string = fs.readFileSync('./index.html', 'utf8')
+     var amount = fs.readFileSync('./db', 'utf8') // 100
+     string = string.replace('&&&amount&&&', amount)
+     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+     response.write(string)
+     response.end()
+   } else if (path === '/pay' && method.toUpperCase() === 'POST') { //toUpperCase 转为大写
+     var amount = fs.readFileSync('./db', 'utf8')
+     var newAmount = amount - 1
+     fs.writeFileSync('./db', newAmount)
+     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+     response.write('付款成功')
+     response.end()
+   } else {
+     response.statusCode = 404
+     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+     response.write('呜呜呜')
+     response.end()
+   } */
+
+  if (path === '/') {
+    response.statusCode = 200
+    var string = fs.readFileSync('./index.html', 'utf8')
+    var amount = fs.readFileSync('./db', 'utf8') // 100
+    string = string.replace('&&&amount&&&', amount)
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
     response.end()
-  }else if(path == '/main.js'){
-    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-    response.write('alert("这是JS执行的")')
+  } else if (path === '/pay') {
+    var amount = fs.readFileSync('./db', 'utf8')
+    amount -= 1
+    fs.writeFileSync('./db', amount)
+    response.setHeader('Content-Type', 'application/javascript')
+    response.write('amount.innerText = ' + amount)
     response.end()
-  }else if(path == '/'){
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.css">' +
-      '</head><body>'  +
-      '<h1>你好</h1>' +
-      '<script src="/main.js"></script>' +
-      '</body></html>')
-    response.end()
-  }else{
+  } else {
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
     response.end()
   }
 
@@ -47,3 +70,5 @@ var server = http.createServer(function(request, response){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
